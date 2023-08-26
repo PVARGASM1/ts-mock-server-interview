d4afd957ab0bd2b6a320d89efffbeb198eb98c60
PVARGASM1 <andrea.vargas18@hotmail.es>
2023-08-26 12:22:41 -0500

    Finished back-end interview MIR

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,63 @@ app.get('/api', (_, res: Response) => {
     res.send('Hello World!');
 });
 
+//GET ALLS TASKS 
+app.get('/api/tasks', (_, res: Response) => {
+
+    res.status(201).json(tasks)
+    
+    res.status(400).json({message: "Tasks not found"})
+    
+ });
+
+ // GET TASK BY ID 
+app.get('/api/tasks/:id', (req:Request, res: Response) => {
+    const { id } = req.params
+    const uniqueTask = tasks.find(task => task.id === parseInt(id));
+         
+    res.status(201).json(uniqueTask)
+
+        res.status(400).json({message: "Task not found"})
+    
+ });
+
+  // POST CREATE NEW TASK
+app.post('/api/tasks', (req:Request, res: Response) => {
+
+    const createNewTasks = req.body
+    tasks.push(createNewTasks)
+         
+    res.status(201).json(tasks);
+   
+        res.status(400).json({message: "Tasks wasn't created successfully"})
+
+ });
+
+  // PUT UPDATE TASK
+  app.put('/api/tasks/:id', (req: Request, res: Response) => {
+    const { id } = req.params
+    const data = req.body
+
+    const findTaskToEdit = tasks.find(task => task.id === parseInt(id))
+
+    findTaskToEdit.todo = data.todo
+
+        res.status(202).json(findTaskToEdit)
+    
+        res.status(403).json({ message: "Error updating the task" })
+
+});
+
+app.delete('/api/tasks/:id', (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    tasks.splice(Number(id) - 1);
+
+    res.status(200).json(tasks);
+
+    res.status(400).json({ message: "Error deleting task" })
+
+});
 
 app.listen(port, () => {
     console.log(`Server Running Up on port ${port}`);
